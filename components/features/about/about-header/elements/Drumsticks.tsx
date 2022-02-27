ac4abcf260b34848e12763005607afadd2ba3f11
Ivica Batinic <ivica.batinic@infinum.hr>
2022-02-27 20:46:33 +0100

    WIP on batak 3d

--- components/features/about/about-header/elements/Drumsticks.tsx
+++ components/features/about/about-header/elements/Drumsticks.tsx
@@ -7,24 +7,45 @@ import { useState, useMemo } from 'react';
 import { useThree } from '@react-three/fiber';
 import { useGLTF, Float } from '@react-three/drei';
 import { LayerMaterial, Base, Depth, Fresnel, Noise } from 'lamina/vanilla';
+import { GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
 
-const colorA = new THREE.Color('#2032A5').convertSRGBToLinear();
+type GLTFResult = GLTF & {
+  nodes: {
+    Meat: THREE.Mesh;
+    Bone: THREE.Mesh;
+  };
+  materials: {
+    MeatMaterial: THREE.MeshStandardMaterial;
+    BoneMaterial: THREE.MeshStandardMaterial;
+  };
+};
+
+const meatBaseColor = new THREE.Color('#E76203').convertSRGBToLinear();
 const colorB = new THREE.Color('#0F1C4D').convertSRGBToLinear();
-const fresnel = new THREE.Color('#E7B473').convertSRGBToLinear();
-const material = new LayerMaterial({
+const meatFresnel = new THREE.Color('#E7B473').convertSRGBToLinear();
+const materialMeat = new LayerMaterial({
   layers: [
-    new Base({ color: colorA }),
-    new Depth({ colorA: colorA, colorB: colorB, alpha: 0.5, mode: 'normal', near: 0, far: 2, origin: [1, 1, 1] }),
-    new Depth({ colorA: 'purple', colorB: colorB, alpha: 0.5, mode: 'add', near: 3, far: 2, origin: [1, 1, 1] }),
-    new Fresnel({ mode: 'add', color: fresnel, intensity: 0.3, power: 2.5, bias: 0.0 }),
+    new Base({ color: meatBaseColor }),
+    // new Depth({ colorA: colorA, colorB: colorB, alpha: 0.5, mode: 'normal', near: 0, far: 2, origin: [1, 1, 1] }),
+    // new Depth({ colorA: 'purple', colorB: colorB, alpha: 0.5, mode: 'add', near: 3, far: 2, origin: [1, 1, 1] }),
+    new Fresnel({ mode: 'add', color: meatFresnel, intensity: 0.3, power: 2.5, bias: 0.0 }),
+    new Noise({ mapping: 'local', type: 'simplex', scale: 1000, colorA: '#ffaf40', colorB: 'black', mode: 'overlay' }),
+  ],
+});
+
+const boneBaseColor = new THREE.Color('#ffffff').convertSRGBToLinear();
+const boneFresnel = new THREE.Color('#000000').convertSRGBToLinear();
+const materialBone = new LayerMaterial({
+  layers: [
+    new Base({ color: boneBaseColor }),
     new Noise({ mapping: 'local', type: 'simplex', scale: 1000, colorA: '#ffaf40', colorB: 'black', mode: 'overlay' }),
   ],
 });
 
 function Drumstick() {
   const { viewport, camera } = useThree();
-  const { nodes } = useGLTF('/3d/drumstick.glb');
-  const [drumstick] = useState(() => nodes[`drumstick`]);
+  const { nodes, materials } = useGLTF('/3d/batak.glb') as GLTFResult;
+
   const [speed] = useState(() => 0.1 + Math.random() / 10);
   const position = useMemo(() => {
     const z = Math.random() * -30;
@@ -34,7 +55,19 @@ function Drumstick() {
 
   return (
     <Float position={position} speed={speed} rotationIntensity={10} floatIntensity={40} dispose={null}>
-      <mesh scale={2} geometry={drumstick.geometry} material={material} />
+      <mesh scale={1} geometry={nodes.Meat.geometry} material={materialMeat} />
+      <mesh scale={1} geometry={nodes.Bone.geometry} material={materialBone} />
+      <group dispose={null}>
+        <mesh castShadow receiveShadow geometry={nodes.Meat.geometry} material={materialMeat} />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Bone.geometry}
+          material={materialBone}
+          position={[0, 1.88, 0.42]}
+          scale={[1, 0.63, 0.94]}
+        />
+      </group>
     </Float>
   );
 }
@@ -49,4 +82,4 @@ export function Drumsticks() {
   );
 }
 
-useGLTF.preload('/3d/drumstick.glb');
+useGLTF.preload('/3d/batak.glb');
