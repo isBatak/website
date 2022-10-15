381b45d4e2d539eaec58710655ddbd7249285caa
Ivica Batinic <ivica.batinic@infinum.hr>
2022-10-15 19:33:03 +0200

    Add og type

--- components/seo.tsx
+++ components/seo.tsx
@@ -12,6 +12,7 @@ const Seo = ({ post }: { post: PostType }) => {
         property="og:image"
         content={encodeURI(`${baseURL}/api/og?title=${post.title}&author=${post.author.name}&createdAt=${createdAt}`)}
       />
+      <meta name="og:type" content="summary" />
     </Head>
   );
 };
