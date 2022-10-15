cc9fe08a7552fa80312567fbd823f1e54dec1aaa
Ivica Batinic <ivica.batinic@infinum.hr>
2022-10-15 19:38:12 +0200

    Add favicon and fix twitter card

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,13 @@ export default class Document extends NextDocument {
       <Html lang="en">
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=optional" rel="stylesheet" />
+          <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff"></meta>
         </Head>
         <body>
           <ColorModeScript />
