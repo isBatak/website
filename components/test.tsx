428de95167231fe52014fa6ef18224311d1d04db
Ivica Batinić <ivicabatinic123@gmail.com>
2024-10-11 09:02:53 +0200

    chore: props error

--- components/test.tsx
+++ components/test.tsx
@@ -1,16 +1,9 @@
-import { DrawerRoot, DrawerRootProps, SlotRecipeProps } from '@chakra-ui/react';
-import { Dialog as ArkDialog } from '@ark-ui/react/dialog';
-import type { Assign } from '@ark-ui/react';
+import { DrawerRoot, DrawerRootProps } from '@chakra-ui/react';
 
-type Slot = SlotRecipeProps<'drawer'>;
-type Test = Assign<ArkDialog.RootBaseProps, SlotRecipeProps<'drawer'>>;
-//    ^?
-
-type Resolve<T> = T extends Function ? T : { [K in keyof T]: T[K] };
-
-type T = Resolve<Slot>;
-//   ^?
-
-export function Test({ children, defaultOpen, ...rest }: DrawerRootProps) {
-  return <DrawerRoot {...rest}>{children}</DrawerRoot>;
+export function Test({ children, onOpenChange, ...rest }: DrawerRootProps) {
+  return (
+    <DrawerRoot onOpenChange={onOpenChange} {...rest}>
+      {children}
+    </DrawerRoot>
+  );
 }
